225feb8bb3ef40ea6fdbf22e6426152eab4e0c59
xy <xxinyitoh@gmail.com>
2025-06-17 20:51:59 +0800

    fix: welcome screen and file paths
    • shifted welcome screen folder to pages component
    • shifted images folder to public
    • updated begin button to clickable link
    • simple testing of links from archive to letter to welcome screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
-import { letters } from './letters';
-import { WelcomeScreen } from './components/WelcomeScreen/WelcomeScreen';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// 2. Import all your page components
+import { WelcomeScreen } from './pages/WelcomeScreen/WelcomeScreen';
+import { CurrentLetterView } from './pages/CurrentLetterView/CurrentLetterView';
+import { ArchiveView } from './pages/ArchiveView/ArchiveView';
+//import { MusicPlayer } from './components/Music/Music';
 
 function App() {
   return (
-    <WelcomeScreen />
+    <Router>
+      {/* components that should appear on all pages */}
+      {/* <Music /> */}
+
+      {/* 4. routes component will manage switching between pages */}
+      <Routes>
+        {/*
+          The "path" is the URL in the browser.
+          The "element" is the component to show for that path.
+        */}
+        <Route path="/" element={<WelcomeScreen />} />
+        <Route path="/letter" element={<CurrentLetterView />} />
+        <Route path="/archive" element={<ArchiveView />} />
+      </Routes>
+    </Router>
   );
 }
 
