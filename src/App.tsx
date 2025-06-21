a80c65929b7fbc13c54097b02e3b45b1c3952360
xy <xxinyitoh@gmail.com>
2025-06-21 17:22:42 +0800

    feat: background music

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,23 @@
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-// 2. Import all your page components
 import { WelcomeScreen } from './pages/WelcomeScreen/WelcomeScreen';
 import { CurrentLetterView } from './pages/CurrentLetterView/CurrentLetterView';
 import { ArchiveView } from './pages/ArchiveView/ArchiveView';
-//import { MusicPlayer } from './components/Music/Music';
+import { Music } from './components/Music/Music';
 
 function App() {
   return (
     <Router>
-      {/* components that should appear on all pages */}
-      {/* <Music /> */}
+      <div>
+        <Routes>
+          <Route path="/" element={<WelcomeScreen />} />
+          <Route path="/letter" element={<CurrentLetterView />} />
+          <Route path="/archive" element={<ArchiveView />} />
+        </Routes>
 
-      {/* 4. routes component will manage switching between pages */}
-      <Routes>
-        {/*
-          The "path" is the URL in the browser.
-          The "element" is the component to show for that path.
-        */}
-        <Route path="/" element={<WelcomeScreen />} />
-        <Route path="/letter" element={<CurrentLetterView />} />
-        <Route path="/archive" element={<ArchiveView />} />
-      </Routes>
+        <Music />
+      </div>
     </Router>
   );
 }
